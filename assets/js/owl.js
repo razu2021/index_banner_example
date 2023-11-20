9c871326f6dfaf0437c6bb7511a8b8b91a71602e
razu2021 <mdrazuhossainraj@gmail.com>
2023-11-20 17:12:55 +0600

    todays work

---
+++ assets/js/owl.js
@@ -0,0 +1,11 @@
+// inedex page banner slider one 
+$(document).ready(function(){
+    $(".index-page-banner-slider-one").owlCarousel({
+        items:1,
+        autoplay:true,
+        loop:true,
+        nav: true,
+        dots:false,
+        navText: ["<span class='prev'>  <  </span>", "<span class='next'> > </span>"]
+    });
+  });
